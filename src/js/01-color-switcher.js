31a649dfd30f10ed0cfb03a42826dff0dfb65a3a
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-01-25 13:53:56 +0200

    add task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let startIntervalId = null;
 
+refs.startBtn.addEventListener('click', bodyChangeColor);
+refs.stopBtn.addEventListener('click', stopbodycolor);
+
+function bodyChangeColor() {
+  startIntervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.setAttribute('disabled', 'disabled');
+}
+function stopbodycolor() {
+  clearInterval(startIntervalId);
+  refs.startBtn.removeAttribute('disabled');
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
